a94f1c3556597b66cf66d0ed9771ff40820f3085
Mehannioui <mehannioui@gmail.com>
2025-11-15 17:17:33 -0500

    Light/Dark Theme

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,5 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import Theme from "./Theme";
+
 const Navbar = () => {
-  return <div className="flex-between">Navbar</div>;
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 shadow-ligh-300 gap-5">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="DevFlow Logo"
+        />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+      <p>Global Search</p>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
